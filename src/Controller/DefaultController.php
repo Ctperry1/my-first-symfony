802c1753a56c78f89a32f8895b2f7d90c2f5a23c
Tyler <ctperry1@yahoo.com>
2021-02-23 16:46:34 -0500

    Updated

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -2,26 +2,36 @@
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 
-class DefaultController {
+class DefaultController extends AbstractController
+{
 
     /**
      * @Route("/")
      */
-    public function homepage(){
+    public function homepage()
+    {
         return new Response('This is some sample text for a test.');
     }
 
     /**
      * @Route("/questions/{slug}")
      */
-    public function show($slug){
-        return new Response(sprintf(
-            'Future page to show the question "%s"!',
-            ucwords(str_replace('-', ' ', $slug))
-        ));
+    public function show($slug)
+    {
+
+        $answers = [
+            'Make sure your cat is sitting purrrfectly still 🤣',
+            'Honestly, I like furry shoes better than MY cat',
+            'Maybe... try saying the spell backwards?',
+        ];
+        return $this->render('question/show.html.twig', [
+            'question' => ucwords(str_replace('-', ' ', $slug)),
+            'answers' => $answers,
+        ]);
     }
 }
