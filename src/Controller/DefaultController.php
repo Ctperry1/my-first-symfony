5619d5755e34c5f22b84053b324062d066670346
Tyler <ctperry1@yahoo.com>
2021-03-03 12:57:44 -0500

    updates

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -1,37 +1,35 @@
 <?php
-
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-
+use Twig\Environment;
 
 class DefaultController extends AbstractController
 {
-
     /**
-     * @Route("/", name="homepage")
+     * @Route("/", name="app_homepage")
      */
-    public function homepage()
+    public function homepage(Environment $twigEnvironment)
     {
+        /*
+        // fun example of using the Twig service directly!
+        $html = $twigEnvironment->render('question/homepage.html.twig');
+        return new Response($html);
+        */
         return $this->render('question/homepage.html.twig');
     }
-
     /**
-     * @Route("/questions/{slug}")
+     * @Route("/questions/{slug}", name="app_question_show")
      */
     public function show($slug)
     {
-
         $answers = [
             'Make sure your cat is sitting purrrfectly still 🤣',
             'Honestly, I like furry shoes better than MY cat',
             'Maybe... try saying the spell backwards?',
         ];
-
-        dump($slug, $this);
-
         return $this->render('question/show.html.twig', [
             'question' => ucwords(str_replace('-', ' ', $slug)),
             'answers' => $answers,
